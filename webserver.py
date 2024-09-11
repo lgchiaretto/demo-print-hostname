033fcaf5d68f3ae162f5e70196e6ce9909c5e03c
Luiz Gustavo Chiaretto <luizgustavo@chiaretto.com.br>
2024-09-10 23:58:19 -0300

    do not print

--- webserver.py
+++ webserver.py
@@ -14,7 +14,6 @@ class CustomHandler(SimpleHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=CustomHandler, port=8080):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print(f"Starting server on port {port}")
     httpd.serve_forever()
 
 if __name__ == '__main__':
